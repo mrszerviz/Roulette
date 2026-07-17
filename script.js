202c4becffaaeb7fcfcee4564e567932e7c50ffa
mrszerviz <milan.deak@proton.me>
2026-07-17 22:39:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,56 +2,41 @@ let money = 1000;
 
 let bet = null;
 
-let numberBet = null;
+let selectedNumber = null;
+
 
 let games=0;
 let wins=0;
 let losses=0;
 
 let totalBet=0;
-let totalWin=0;
+let totalReturn=0;
 
 
 
-const reds=[
-1,3,5,7,9,12,
-14,16,18,19,
-21,23,25,27,
-30,32,34,36
+const redNumbers=[
+
+1,3,5,7,9,
+12,14,16,18,
+19,21,23,25,
+27,30,32,34,36
+
 ];
 
 
 
-function createNumbers(){
+function chooseNumber(number){
 
-let box=document.getElementById("numbers");
-
-
-for(let i=0;i<=36;i++){
-
-let b=document.createElement("button");
-
-b.innerHTML=i;
-
-b.onclick=()=>{
-
-numberBet=i;
+selectedNumber=number;
 
 bet=null;
 
-document.getElementById("message").innerHTML=
-"Szám fogadás: "+i;
+
+document.getElementById("message")
+.innerHTML=
+"Szám: "+number;
 
 }
-
-box.appendChild(b);
-
-}
-
-}
-
-
-createNumbers();
 
 
 
@@ -59,23 +44,26 @@ function setBet(type){
 
 bet=type;
 
-numberBet=null;
+selectedNumber=null;
 
-document.getElementById("message").innerHTML=
+
+document.getElementById("message")
+.innerHTML=
 "Fogadás: "+type;
 
 }
 
 
 
+function color(number){
 
-function getColor(num){
-
-if(num===0)
+if(number===0)
 return "green";
 
-if(reds.includes(num))
+
+if(redNumbers.includes(number))
 return "red";
+
 
 return "black";
 
@@ -88,13 +76,14 @@ function spin(){
 
 
 let amount=
+
 Number(
-document.getElementById("betAmount").value
+document.getElementById("amount").value
 );
 
 
 
-if(!bet && numberBet===null){
+if(!bet && selectedNumber===null){
 
 alert("Válassz fogadást!");
 
@@ -122,19 +111,27 @@ games++;
 
 
 
-let wheel=document.getElementById("wheel");
+let wheel=
+
+document.getElementById("wheel");
 
 
-let rotation=
-Math.floor(Math.random()*3600)+720;
+
+let rotate=
+
+Math.floor(Math.random()*7200)+1440;
+
 
 
 wheel.style.transform=
-`rotate(${rotation}deg)`;
+
+`rotate(${rotate}deg)`;
+
 
 
 
 let number=
+
 Math.floor(Math.random()*37);
 
 
@@ -147,18 +144,19 @@ document.getElementById("resultNumber")
 
 
 
-let color=getColor(number);
-
-
 let win=false;
 
 let multiplier=0;
 
 
 
-if(numberBet!==null){
+let c=color(number);
 
-if(number===numberBet){
+
+
+if(selectedNumber!==null){
+
+if(number===selectedNumber){
 
 win=true;
 
@@ -170,40 +168,40 @@ multiplier=35;
 
 
 
-if(bet==="red" && color==="red"){
+if(bet==="red" && c==="red"){
 
 win=true;
+
 multiplier=1;
 
 }
 
 
 
-if(bet==="black" && color==="black"){
+if(bet==="black" && c==="black"){
 
 win=true;
+
 multiplier=1;
 
 }
 
 
 
-if(bet==="even" &&
-number!==0 &&
-number%2===0){
+if(bet==="even" && number!==0 && number%2===0){
 
 win=true;
+
 multiplier=1;
 
 }
 
 
 
-if(bet==="odd" &&
-number!==0 &&
-number%2!==0){
+if(bet==="odd" && number!==0 && number%2!==0){
 
 win=true;
+
 multiplier=1;
 
 }
@@ -212,32 +210,45 @@ multiplier=1;
 
 if(win){
 
+
 let prize=
+
 amount*(multiplier+1);
 
 
 money+=prize;
 
-totalWin+=prize;
+totalReturn+=prize;
 
 wins++;
 
 
 document.getElementById("message")
 .innerHTML=
-"🎉 NYERTÉL! +" + prize+" Ft";
+
+"🎉 NYERTÉL! "+
+number+
+" +"+
+prize+
+" Ft";
 
 
 }
 
 else{
 
+
 losses++;
+
 
 document.getElementById("message")
 .innerHTML=
-"❌ Vesztettél ("+
-number+" "+color+")";
+
+"❌ Vesztettél! "+
+number+
+" ("+
+c+
+")";
 
 
 }
@@ -247,15 +258,16 @@ number+" "+color+")";
 update();
 
 
-},4000);
-
+},5000);
 
 
 }
 
 
 
+
 function update(){
+
 
 document.getElementById("money")
 .innerHTML=money;
@@ -280,13 +292,15 @@ let rtp=0;
 if(totalBet>0){
 
 rtp=
-(totalWin/totalBet)*100;
+
+(totalReturn/totalBet)*100;
 
 }
 
 
 document.getElementById("rtp")
 .innerHTML=
+
 rtp.toFixed(2);
 
 
